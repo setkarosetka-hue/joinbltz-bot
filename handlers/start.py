5d637f91b742b4eb9b46c9adb7b36d337f2bdce4
setkarosetka-hue <setkarosetka@gmail.com>
2026-07-17 17:00:05 +0200

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,33 +1,24 @@
 from aiogram import Router
-from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import Message, FSInputFile
 from aiogram.filters import Command
+
+from keyboards.user import main_menu
 
 router = Router()
 
 
 @router.message(Command("start"))
-async def start_command(message: Message):
+async def start(message: Message):
 
-    keyboard = InlineKeyboardMarkup(
-        inline_keyboard=[
-            [
-                InlineKeyboardButton(
-                    text="📋 Подать заявку",
-                    callback_data="apply"
-                )
-            ],
-            [
-                InlineKeyboardButton(
-                    text="📜 Правила BLTZ",
-                    callback_data="rules"
-                )
-            ]
-        ]
+    photo = FSInputFile(
+        "images/bltz_logo.png"
     )
 
-    await message.answer(
+    await message.answer_photo(
+        photo=photo,
+        caption=
         "⚡ <b>Добро пожаловать в BLTZ!</b>\n\n"
-        "🛡️ Здесь ты можешь подать заявку на вступление в клан.\n\n"
-        "Нажми кнопку ниже, чтобы начать.",
-        reply_markup=keyboard
+        "🛡️ Бот для вступления в наш проект.\n\n"
+        "Нажми кнопку ниже, чтобы подать заявку.",
+        reply_markup=main_menu()
     )
