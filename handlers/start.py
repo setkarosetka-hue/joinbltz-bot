0905577fa037a34091b1e4a2a77f3487a43a0bc5
setkarosetka-hue <setkarosetka@gmail.com>
2026-07-17 17:07:02 +0200

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -3,6 +3,8 @@ from aiogram.types import Message, FSInputFile
 from aiogram.filters import Command
 
 from keyboards.user import main_menu
+from database.sqlite import get_setting
+
 
 router = Router()
 
@@ -10,15 +12,24 @@ router = Router()
 @router.message(Command("start"))
 async def start(message: Message):
 
+    # Получаем приветствие из базы
+    welcome = await get_setting("welcome")
+
+    if not welcome:
+        welcome = (
+            "⚡ <b>Добро пожаловать в BLTZ!</b>\n\n"
+            "🛡️ Бот для вступления в наш клан.\n\n"
+            "Нажми кнопку ниже, чтобы подать заявку."
+        )
+
+
     photo = FSInputFile(
         "images/bltz_logo.png"
     )
 
+
     await message.answer_photo(
         photo=photo,
-        caption=
-        "⚡ <b>Добро пожаловать в BLTZ!</b>\n\n"
-        "🛡️ Бот для вступления в наш проект.\n\n"
-        "Нажми кнопку ниже, чтобы подать заявку.",
+        caption=welcome,
         reply_markup=main_menu()
     )
