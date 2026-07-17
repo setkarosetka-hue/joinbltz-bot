9e3f90376d05608267e4fbb7351e88feb68676b2
setkarosetka-hue <setkarosetka@gmail.com>
2026-07-17 16:40:41 +0200

    Update admin.py

--- keyboards/admin.py
+++ keyboards/admin.py
@@ -1,1 +1,35 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+
+def admin_menu():
+
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(
+                    text="📋 Заявки",
+                    callback_data="applications"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    text="📢 Рассылка",
+                    callback_data="broadcast"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    text="⚙️ Редактор приветствия",
+                    callback_data="welcome_edit"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    text="📊 Статистика",
+                    callback_data="stats"
+                )
+            ]
+        ]
+    )
+
+    return keyboard
