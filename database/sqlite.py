efcec1f22a726212885a804b6e950a9be56c563a
setkarosetka-hue <setkarosetka@gmail.com>
2026-07-17 17:03:05 +0200

    Update sqlite.py

--- database/sqlite.py
+++ database/sqlite.py
@@ -1,67 +1,33 @@
-import aiosqlite
-
-
-DB_NAME = "database.db"
-
-
-async def create_db():
-
-    async with aiosqlite.connect(DB_NAME) as db:
-
-        await db.execute("""
-        CREATE TABLE IF NOT EXISTS users(
-            id INTEGER PRIMARY KEY,
-            username TEXT
-        )
-        """)
-
-
-        await db.execute("""
-        CREATE TABLE IF NOT EXISTS applications(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            user_id INTEGER,
-            roblox TEXT,
-            age TEXT,
-            about TEXT,
-            status TEXT
-        )
-        """)
-
-
-        await db.execute("""
-        CREATE TABLE IF NOT EXISTS settings(
-            name TEXT PRIMARY KEY,
-            value TEXT
-        )
-        """)
-
-
-        await db.commit()
-
-
-
-async def add_application(
-        user_id,
-        roblox,
-        age,
-        about
-):
+async def set_setting(name, value):
 
     async with aiosqlite.connect(DB_NAME) as db:
 
         await db.execute(
             """
-            INSERT INTO applications
-            (user_id, roblox, age, about, status)
-            VALUES (?, ?, ?, ?, ?)
+            INSERT INTO settings(name, value)
+            VALUES(?, ?)
+            ON CONFLICT(name)
+            DO UPDATE SET value=excluded.value
             """,
-            (
-                user_id,
-                roblox,
-                age,
-                about,
-                "Ожидание"
-            )
+            (name, value)
         )
 
         await db.commit()
+
+
+
+async def get_setting(name):
+
+    async with aiosqlite.connect(DB_NAME) as db:
+
+        cursor = await db.execute(
+            "SELECT value FROM settings WHERE name=?",
+            (name,)
+        )
+
+        result = await cursor.fetchone()
+
+        if result:
+            return result[0]
+
+        return None
