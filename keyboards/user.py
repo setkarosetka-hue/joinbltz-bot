c94dd1df2016712a411cb555f6eacc62346c34b2
setkarosetka-hue <setkarosetka@gmail.com>
2026-07-17 16:40:28 +0200

    Update user.py

--- keyboards/user.py
+++ keyboards/user.py
@@ -1,1 +1,23 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+
+def main_menu():
+
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(
+                    text="📋 Подать заявку",
+                    callback_data="apply"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    text="📜 Правила BLTZ",
+                    callback_data="rules"
+                )
+            ]
+        ]
+    )
+
+    return keyboard
